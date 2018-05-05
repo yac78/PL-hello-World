83061efa0cf4ecb79bdac9c53463eba515766a40
Yacoubi <info@yacoubi.de>
2018-05-05 16:24:58 +0200

    Hello World
    Version1 Hello World

---
+++ src/Providers/HelloWorldRouteServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace HelloWorld\Providers;
+
+
+use Plenty\Plugin\RouteServiceProvider;
+use Plenty\Plugin\Routing\Router;
+
+class HelloWorldRouteServiceProvider extends RouteServiceProvider
+{
+    public function map(Router $router)
+    {
+        $this->getApplication()->register(HelloWorldRouteServiceProvider::class);
+        $router->get('hello','HelloWorld\Controllers\ContentController@sayHello');
+    }
+    
+}
